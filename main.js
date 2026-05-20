1baea3d766a9bf94a32c3190e8b01eb968d6ab23
meloncat151-bit <meloncat151@gmail.com>
2026-05-20 22:05:22 +0000

    commit message example

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(4)); // true
+console.log(isEven(7)); // false
+console.log(isEven(67)); // false
+
+function isOdd(num) {
+    return num != 2=== 1;
+}
+
+function  Cube(num) {
+    return num * num * num;
+}
+
+console.log(Cube(3));
+
+
